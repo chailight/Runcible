edaf35f25da7888b13e468ece9f880889bcfb826
chailight <markp@chailight.com>
2017-04-15 13:10:42 +0000

    Initial checkin

---
+++ clocks.py
@@ -0,0 +1,146 @@
+import asyncio
+import aiosc
+import time
+import collections
+import queue
+#import rtmidi2
+
+try:
+    import rtmidi2
+except ImportError:
+    pass
+
+class RtMidiClock:
+    def __init__(self, loop=None):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        self._loop = loop
+
+        self.ticks = -1
+        self.bpm = 120
+        self._last_tick = time.time()
+        self._tick_intervals = collections.deque(maxlen=96)
+
+        self._tick_event = asyncio.Event()
+        self._tick_queue = queue.Queue()
+
+        #self._rtmin = rtmidi2.MidiIn("RtMidiClock")
+        self._rtmin = rtmidi2.MidiIn()
+        self._rtmin.ignore_types(True, False, True)
+        self._rtmin.callback = self._on_midi_message
+
+        self.stopped = False
+        self.start()
+
+    def start(self):
+        self._read_ticks_task = asyncio.async(self._read_ticks())
+        #self._rtmin.open_virtual_port("RtMidi In Sync")
+        print(rtmidi2.get_in_ports())
+        print(rtmidi2.get_out_ports())
+        self._rtmin.open_port(1)
+
+    def stop(self):
+        self._rtmin.close_port()
+        self._read_ticks_task.cancel()
+
+    def _on_midi_message(self, msg, time):
+        msgtype = msg[0]
+        if msgtype == 248:
+            # tick
+            self._tick_queue.put(248)
+        elif msgtype == 250:
+            # start
+            self.ticks = -1
+            self.stopped = False
+        elif msgtype == 251:
+            # continue
+            self.stopped = False
+        elif msgtype == 252:
+            # stop
+            self.stopped = True
+
+    @asyncio.coroutine
+    def _read_ticks(self):
+        while True:
+            try:
+                b = yield from self._loop.run_in_executor(None, self._tick_queue.get, True, 0.250)
+                if not self.stopped:
+                    self.ticks += 1
+
+                    # update bpm
+                    current_tick = time.time()
+                    self._tick_intervals.append(current_tick - self._last_tick)
+                    self.bpm = 1 / (sum(self._tick_intervals) / len(self._tick_intervals)) / 24 * 60
+                    self._last_tick = current_tick
+
+                    self._tick_event.set()
+                    self._tick_event.clear()
+            except queue.Empty:
+                pass
+            except asyncio.CancelledError:
+                return
+
+    @asyncio.coroutine
+    def sync(self, q=1):
+        yield from self._tick_event.wait()
+        while self.ticks % q != 0:
+            yield from self._tick_event.wait()
+        return self.ticks
+
+
+class FooClock(aiosc.OSCProtocol):
+    def __init__(self):
+        super().__init__(handlers={
+            '/bang': lambda addr, path, *args: self.__bang_handler(),
+            '/start': lambda addr, path, *args: self.__start_handler(),
+        })
+        self.__bang_event = asyncio.Event()
+
+        self.ticks = 0
+        self.last_tick = time.time()
+        self.bang_intervals = collections.deque(maxlen=96)
+        self.bpm = 120
+
+    def __bang_handler(self):
+        self.ticks += 1
+
+        current_tick = time.time()
+        self.bang_intervals.append(current_tick - self.last_tick)
+        self.bpm = 1 / (sum(self.bang_intervals) / len(self.bang_intervals)) / 24 * 60
+        self.last_tick = current_tick
+
+        self.__bang_event.set()
+        self.__bang_event.clear()
+
+    def __start_handler(self):
+        self.ticks = 0
+
+    @asyncio.coroutine
+    def sync(self, q=1):
+        yield from self.__bang_event.wait()
+        while self.ticks % q != 0:
+            yield from self.__bang_event.wait()
+        return self.ticks
+
+class InaccurateTempoClock:
+    def __init__(self, tempo):
+        self.tempo = tempo
+        self.ticks = 0
+        self.__bang_event = asyncio.Event()
+        self.__ticktask = asyncio.async(self.__tick())
+        self.bpm = tempo
+
+    @asyncio.coroutine
+    def __tick(self):
+        while True:
+            self.ticks += 1
+            self.__bang_event.set()
+            self.__bang_event.clear()
+            yield from asyncio.sleep(60 / self.tempo / 4 / 24)
+
+    @asyncio.coroutine
+    def sync(self, q=1):
+        yield from self.__bang_event.wait()
+        while self.ticks % q != 0:
+            yield from self.__bang_event.wait()
+        return self.ticks
---
+++ runcible.py
@@ -0,0 +1,412 @@
+#! /usr/bin/env python3
+#RUNCIBLE - a raspberry pi / python sequencer for spanned 40h monomes inspired by Ansible Kria
+#TODO:
+#fix grid 2 input limits
+#remove extraneous note display on grid 1 
+#check for cutting / looping input on both grids
+#pass in midi out port from main loop
+#add pages for duration, velocity, octave and probability
+#- this might be tricky - requiring message passing between grids
+#- is there access to a global mode value?
+#add presets: store and recall - likewise a global preset value? 
+#add persistence of presets
+#add scale setting for both grids - global value?
+
+import asyncio
+import monome
+import clocks
+import synths
+import pygame
+import pygame.midi
+from pygame.locals import *
+
+def cancel_task(task):
+    if task:
+        task.cancel()
+
+class GridSeq1(monome.Monome):
+    def __init__(self, clock, ticks, midi_out,channel_out,clock_out,page):
+        super().__init__('/monome')
+        self.clock = clock
+        self.ticks = ticks 
+        self.midi_out = midi_out
+        self.channel = channel_out
+        self.clock_ch= clock_out
+        self.page = page 
+
+    def ready(self):
+        print ("using grid on port :%s" % self.id)
+        self.current_pos = 0
+        self.step = [[0 for col in range(self.width)] for row in range(self.height)]
+        self.play_position = 0
+        self.next_position = 0
+        self.cutting = False
+        self.loop_start = 0
+        self.loop_end = self.width - 1
+        self.keys_held = 0
+        self.key_last = 0
+        #pygame.init()
+        #pygame.midi.init()
+        #self.midiport = 2
+        #print ("using output_id :%s:" % self.midiport)
+        #self.midi_out = pygame.midi.Output(self.midiport, 0)
+        asyncio.async(self.play())
+
+    def disconnect(self):
+        self.led_all(0)
+
+    @asyncio.coroutine
+    def play(self):
+        self.current_pos = yield from self.clock.sync()
+        self.play_position = (self.current_pos//self.ticks)%16
+        while True:
+            if ((self.current_pos//self.ticks)%16) < 8:
+                #print("G1:",(self.current_pos//self.ticks)%16)
+                self.draw()
+                # TRIGGER SOMETHING
+                for y in range(self.height):
+                    #print("y:",y, "pos:", self.play_position)
+                    if self.step[y][self.play_position] == 1:
+                        #print("Grid 1:", self.play_position,abs(y-7))
+                        asyncio.async(self.trigger(abs(y-7)))
+
+#                if self.cutting:
+#                    self.play_position = self.next_position
+#                elif self.play_position == self.width - 1:
+#                    self.play_position = 0
+#                elif self.play_position == self.loop_end:
+#                    self.play_position = self.loop_start
+#                else:
+#                    self.play_position += 1
+
+                self.cutting = False
+            else:
+                #buffer = monome.LedBuffer(self.width, self.height)
+                #buffer.led_level_set(0, 0, 0)
+                self.draw()
+
+            #yield from asyncio.sleep(0.1)
+            asyncio.async(self.clock_out())
+            yield from self.clock.sync(self.ticks)
+            self.current_pos = yield from self.clock.sync()
+            self.play_position = (self.current_pos//self.ticks)%16
+
+    def gridToSpan(self,x,y):
+        #return [abs(y-7),x]
+        return [abs(x-7),abs(y-7)]
+         
+    def spanToGrid(self,x,y):
+        #return [y,abs(x-7)]
+        return [abs(x-7),abs(y-7)]
+
+    @asyncio.coroutine
+    def trigger(self, i):
+        #print("Grid1", i)
+        self.current_note = 40+i
+        #print("G1: note: " + str(self.current_note) + " channel: " + str(self.channel))
+        self.midi_out.note_on(self.current_note, 60, self.channel)
+        yield from self.clock.sync(self.ticks)
+        #yield from asyncio.sleep(0.01)
+        self.midi_out.note_off(self.current_note, 0, self.channel)
+
+    @asyncio.coroutine
+    def clock_out(self):
+        #print("Grid1", i)
+        #self.midi_out.note_on(40, 60, self.clock_ch)
+        #print("G1: clock on "  + " channel: " + str(self.clock_ch))
+        yield from self.clock.sync(self.ticks)
+        #yield from asyncio.sleep(0.01)
+        #self.midi_out.note_off(40, 0, self.clock_ch)
+        #print("G1: clock off "  + " channel: " + str(self.clock_ch))
+
+    def draw(self):
+        buffer = monome.LedBuffer(self.width, self.height)
+
+        # display steps
+        for x in range(self.width):
+            # highlight the play position
+            if x == self.play_position:
+                highlight = 4
+            else:
+                highlight = 0
+
+            for y in range(self.height):
+                render_pos = self.spanToGrid(x,y)
+                buffer.led_level_set(render_pos[0], render_pos[1], self.step[y][x] * 11 + highlight)
+
+        # draw trigger bar and on-states
+#        for x in range(self.width):
+#            buffer.led_level_set(x, 6, 4)
+
+#        for y in range(6):
+#            if self.step[y][self.play_position] == 1:
+#                buffer.led_level_set(self.play_position, 6, 15)
+
+        # draw play position
+        #current_pos = yield from self.clock.sync()
+        #print("G1:",(self.current_pos//self.ticks)%16)
+        render_pos = self.spanToGrid(self.play_position, 0)
+        if ((self.current_pos//self.ticks)%16) < 8:
+ #           print("Pos",self.play_position)
+            buffer.led_level_set(render_pos[0], render_pos[1], 15)
+        else:
+            buffer.led_level_set(render_pos[0], render_pos[1], 0)
+
+
+        # update grid
+        buffer.render(self)
+
+    def grid_key(self, grid_x, grid_y, s):
+        corrected=self.gridToSpan(grid_x,grid_y)
+        x = corrected[0]
+        y = corrected[1]
+        # toggle steps
+        if s == 1 and y > 0:
+            self.step[y][x] ^= 1
+            self.draw()
+        # cut and loop
+        elif y == 0:
+            self.keys_held = self.keys_held + (s * 2) - 1
+            # cut
+            if s == 1 and self.keys_held == 1:
+                self.cutting = True
+                self.next_position = x
+                self.key_last = x
+            # set loop points
+            elif s == 1 and self.keys_held == 2:
+                self.loop_start = self.key_last
+                self.loop_end = x
+
+class GridSeq2(monome.Monome):
+    def __init__(self,clock,ticks,midi_out,channel_out,clock_out,page):
+        super().__init__('/monome')
+        self.clock = clock
+        self.ticks = ticks 
+        self.midi_out = midi_out
+        self.channel = channel_out
+        self.clock_out= clock_out 
+        self.page = page 
+
+    def ready(self):
+        print ("using grid on port :%s" % self.id)
+        self.current_pos = 0
+        self.step = [[0 for row in range(self.width)] for col in range(self.height)]
+        self.play_position = 0
+        self.next_position = 0
+        self.cutting = False
+        self.loop_start = 0
+        self.loop_end = self.width - 1
+        self.keys_held = 0
+        self.key_last = 0
+        self.x_offset=0
+        #pygame.init()
+        #pygame.midi.init()
+        #self.midiport = 2
+        #print ("using output_id :%s:" % self.midiport)
+        #self.midi_out = pygame.midi.Output(self.midiport, 0)
+        asyncio.async(self.play())
+
+    def disconnect(self):
+        self.led_all(0)
+
+    @asyncio.coroutine
+    def play(self):
+        self.current_pos = yield from self.clock.sync()
+        self.play_position = ((self.current_pos//self.ticks)%16)-8
+        while True:
+            #print((current_pos//self.ticks)%4)
+            if ((self.current_pos//self.ticks)%16) > 7:
+                self.draw()
+                # TRIGGER SOMETHING
+                #print("G2:",self.play_position)
+                for y in range(self.height):
+                    if self.step[y][self.play_position] == 1:
+                        #print("Grid 2:", self.play_position,y)
+                        asyncio.async(self.trigger(y))
+
+                #print("G2:",(current_pos//self.ticks)%16)
+                #if self.cutting:
+                #    self.play_position = self.next_position
+                #elif self.play_position == self.width - 1:
+                #    self.play_position = 0
+                #elif self.play_position == self.loop_end:
+                #    self.play_position = self.loop_start
+                #else:
+                #    self.play_position += 1
+
+                self.cutting = False
+            else:
+                #buffer = monome.LedBuffer(self.width, self.height)
+                #buffer.led_level_set(0, 0, 0)
+                #buffer.render(self)
+                self.draw()
+
+            #yield from asyncio.sleep(0.2)
+            yield from self.clock.sync(6)
+            self.current_pos = yield from self.clock.sync()
+            self.play_position = ((self.current_pos//self.ticks)%16)-8
+
+    @asyncio.coroutine
+    def trigger(self, i):
+        #print("Grid2", i)
+        self.current_note = 40+i
+        self.midi_out.note_on(self.current_note, 60, self.channel)
+        yield from self.clock.sync(self.ticks)
+        #yield from asyncio.sleep(0.01)
+        self.midi_out.note_off(self.current_note, 0, self.channel)
+
+    def draw(self):
+        buffer = monome.LedBuffer(self.width, self.height)
+
+        # display steps
+        for y in range(self.width):
+            # highlight the play position
+            if y == self.play_position:
+                highlight = 4
+            else:
+                highlight = 0
+
+            for x in range(self.height):
+                buffer.led_level_set(y, x, self.step[y][x] * 11 + highlight)
+
+        # draw trigger bar and on-states
+        #for y in range(self.width):
+        #    buffer.led_level_set(1, y, 4)
+
+        #for x in range(6):
+        #    if self.step[x][self.play_position] == 1:
+        #        buffer.led_level_set(1, self.play_position, 15)
+
+        # draw play position
+        #print("G2:",(self.current_pos//self.ticks)%16)
+        if ((self.current_pos//self.ticks)%16) > 7:
+            buffer.led_level_set(7, self.play_position, 15)
+        else:
+            buffer.led_level_set(7, self.play_position, 0)
+
+        # update grid
+        buffer.render(self)
+
+    def gridToSpan(self,x,y):
+        return [abs(y+self.x_offset),abs(x)]
+
+    def spanToGrid(self,x,y):
+        return [abs(y-7),abs(x-self.x_offset)]
+         
+    def grid_key(self, grid_x, grid_y, s):
+        corrected=self.gridToSpan(grid_x,grid_y)
+        x = corrected[0]
+        y = corrected[1]
+        #print (x,y)
+        # toggle steps
+        if s == 1 and y < 7:
+            self.step[y][x] ^= 1
+            self.draw()
+        # cut and loop
+        elif y == 7:
+            self.keys_held = self.keys_held + (s * 2) - 1
+            # cut
+            if s == 1 and self.keys_held == 1:
+                self.cutting = True
+                self.next_position = x
+                self.key_last = x
+            # set loop points
+            elif s == 1 and self.keys_held == 2:
+                self.loop_start = self.key_last
+                self.loop_end = x
+
+class Test1(monome.Monome):
+    def __init__(self):
+        super().__init__('/hello')
+
+    def ready(self):
+        self.x_offset=0
+
+    def gridToSpan(self,x,y):
+        #return [abs(y-7),x]
+        return [abs(x-7),abs(y-7)]
+         
+    def spanToGrid(self,x,y):
+        #return [y,abs(x-7)]
+        return [abs(x-7),abs(y-7)]
+         
+    def grid_key(self, x, y, s):
+        self.led_set(x, y, s)
+        span_coord = self.gridToSpan(x,y) 
+        print("grid 1: ", x,y, span_coord, self.spanToGrid(span_coord[0],span_coord[1]))
+
+class Test2(monome.Monome):
+    def __init__(self):
+        super().__init__('/hello')
+
+    def ready(self):
+        self.x_offset=8
+
+    def gridToSpan(self,x,y):
+        return [abs(y+self.x_offset),abs(x-7)]
+
+    def spanToGrid(self,x,y):
+        return [abs(y-7),abs(x-self.x_offset)]
+         
+    def grid_key(self, x, y, s):
+        self.led_set(x, y, s)
+        span_coord = self.gridToSpan(x,y) 
+        print("grid 2: ", x,y, span_coord, self.spanToGrid(span_coord[0],span_coord[1]))
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+    pygame.init()
+    pygame.midi.init()
+    device_count = pygame.midi.get_count()
+    #print (pygame.midi.get_default_output_id())
+    midiport = 0
+    clock_out = 3
+    info = list()
+    for i in range(device_count):
+       info = pygame.midi.get_device_info(i)
+       print (str(i) + ": " + str(info[1]) + " " + str(info[2]) + " " + str(info[3]))
+       if 'MIDI 6' in str(info[1]) and info[3] == 1: 
+           midiport = i 
+       #if 'MIDI 1' in str(info[1]) and info[3] == 0: 
+       #   clock_out = i 
+    print ("using output_id : %s " % midiport)
+    midi_out = pygame.midi.Output(midiport, 0)
+    print ("using clock source: %s " % clock_out)
+    channel_out = 3
+    page = 1
+    #midi_out=None
+
+    # create clock
+    #coro = loop.create_datagram_endpoint(clocks.FooClock, local_addr=('127.0.0.1', 9000))
+    #transport, clock = loop.run_until_complete(coro)
+    #clock = clocks.InaccurateTempoClock(120)
+    #g1 = lambda: GridSeq1(clock,24,midi_out,channel_out,clock_out,page)
+    #g2 = lambda: GridSeq2(clock,24,midi_out,channel_out,clock_out,page)
+
+    clock = clocks.RtMidiClock()
+    g1 = lambda: GridSeq1(clock,6,midi_out,channel_out,clock_out,page)
+    g2 = lambda: GridSeq2(clock,6,midi_out,channel_out,clock_out,page)
+
+    #g1 = lambda: Test1()
+    #g2 = lambda: Test2()
+    coro = monome.create_serialosc_connection({
+          'm40h-001': g1,
+          'm40h-002': g2,
+    }, loop=loop)
+
+    # create synth
+#    coro = loop.create_datagram_endpoint(synths.Renoise, local_addr=('127.0.0.1', 0), remote_addr=('127.0.0.1', 8001))
+#    transport, renoise = loop.run_until_complete(coro)
+
+#    coro = monome.create_serialosc_connection(lambda: Flin(clock, renoise, 0))
+    serialosc = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        for apps in serialosc.app_instances.values():
+            for app in apps:
+                app.disconnect()
+        print('kthxbye')
+
