3d541d2a725bdf21989a377c810fe1c199677ae8
Mark Pedersen <markp@ishq.local>
2017-04-25 17:30:46 +1000

    basic sequencer test

--- runcible.py
+++ runcible.py
@@ -22,6 +22,95 @@ def cancel_task(task):
     if task:
         task.cancel()
 
+
+class GridStudies(spanned_monome.VirtualGrid):
+    def __init__(self):
+        super().__init__('/monome')
+
+    def ready(self):
+        self.step = [[0 for col in range(self.width)] for row in range(6)]
+        self.play_position = 0
+        self.next_position = 0
+        self.cutting = False
+        self.loop_start = 0
+        self.loop_end = self.width - 1
+        self.keys_held = 0
+        self.key_last = 0
+
+        asyncio.async(self.play())
+
+    @asyncio.coroutine
+    def play(self):
+        while True:
+            if self.cutting:
+                self.play_position = self.next_position
+            elif self.play_position == self.width - 1:
+                self.play_position = 0
+            elif self.play_position == self.loop_end:
+                self.play_position = self.loop_start
+            else:
+                self.play_position += 1
+
+            # TRIGGER SOMETHING
+            for y in range(6):
+                if self.step[y][self.play_position] == 1:
+                    self.trigger(y)
+
+            self.cutting = False
+            self.draw()
+
+            yield from asyncio.sleep(0.1)
+
+    def trigger(self, i):
+        print("triggered", i)
+
+    def draw(self):
+        buffer = monome.LedBuffer(self.width, self.height)
+
+        # display steps
+        for x in range(self.width):
+            # highlight the play position
+            if x == self.play_position:
+                highlight = 4
+            else:
+                highlight = 0
+
+            for y in range(6):
+                buffer.led_level_set(x, y, self.step[y][x] * 11 + highlight)
+
+        # draw trigger bar and on-states
+        for x in range(self.width):
+            buffer.led_level_set(x, 6, 4)
+
+        for y in range(6):
+            if self.step[y][self.play_position] == 1:
+                buffer.led_level_set(self.play_position, 6, 15)
+
+        # draw play position
+        buffer.led_level_set(self.play_position, 7, 15)
+
+        # update grid
+        buffer.render(self)
+
+    def grid_key(self, x, y, s):
+        # toggle steps
+        if s == 1 and y < 6:
+            self.step[y][x] ^= 1
+            self.draw()
+        # cut and loop
+        elif y == 7:
+            self.keys_held = self.keys_held + (s * 2) - 1
+            # cut
+            if s == 1 and self.keys_held == 1:
+                self.cutting = True
+                self.next_position = x
+                self.key_last = x
+            # set loop points
+            elif s == 1 and self.keys_held == 2:
+                self.loop_start = self.key_last
+                self.loop_end = x
+
+
 #the sequencer logic, based on ansible kria
 class Runcible(spanned_monome.VirtualGrid):
     def __init__(self, clock, ticks, midi_out,channel_out,clock_out,other):
@@ -152,7 +241,7 @@ class Runcible(spanned_monome.VirtualGrid):
 
         # draw play position
         #current_pos = yield from self.clock.sync()
-        #print("G1:",(self.current_pos//self.ticks)%16)
+        print("runcible:",(self.current_pos//self.ticks)%16)
         render_pos = self.spanToGrid(self.play_position, 0)
         if ((self.current_pos//self.ticks)%16) < 8:
  #           print("Pos",self.play_position)
@@ -327,6 +416,7 @@ if __name__ == '__main__':
 #    r1 = lambda: Test1() 
 #    r2 = lambda: Test2() 
     sg1 = lambda: Test3() 
+    sg2 = lambda: GridStudies() 
 
     #g1 = lambda: Test1()
     #g2 = lambda: Test2()
@@ -337,7 +427,7 @@ if __name__ == '__main__':
 
     #coro, g1_coro  = monome.create_spanned_serialosc_connection({
     coro = spanned_monome.create_spanned_serialosc_connection({
-          'runcible': g1,
+          'runcible': sg2,
     }, loop=loop)
 
     # create synth
