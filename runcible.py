caa786d11622c422f2d18b642c668b978455f466
Mark Pedersen <markp@ishq.local>
2017-04-30 20:45:23 +1000

    fixes

--- runcible.py
+++ runcible.py
@@ -319,7 +319,8 @@ class Runcible(spanned_monome.VirtualGrid):
                     #fill a column bottom up in the x position
                     current_oct = self.current_pattern.tracks[0].octave[x]
                     if current_oct >= 0:
-                        for i in range (4-current_oct,1): 
+                        print("start = ", 1, "end = ", 4-current_oct)
+                        for i in range (1,4-current_oct):
                             buffer.led_level_set(x, i, 15)
                             print("current oct: ", current_oct, " drawing in row: ", i)
                         #for i in range (current_oct+3,1): #ignore top row
