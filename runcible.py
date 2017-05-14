d79fc7871e4da3d637eb0b0aa62a0f16a0a36f8f
Mark Pedersen <markp@ishq.local>
2017-05-15 00:16:14 +1000

    fixes to resolution - ticks / 3

--- runcible.py
+++ runcible.py
@@ -279,7 +279,7 @@ class Runcible(spanned_monome.VirtualGrid):
 
             asyncio.async(self.trigger())
             #asyncio.async(self.clock_out())
-            yield from self.clock.sync(self.ticks//6)
+            yield from self.clock.sync(self.ticks//3)
             self.current_pos = yield from self.clock.sync()
             for track in self.current_pattern.tracks:
                 track.loop_length = abs(track.loop_end - track.loop_start)+1
