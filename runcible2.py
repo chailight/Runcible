dfcc5b91461e7b6a8526c19a14c6b5c5356084fc
Mark Pedersen <markp@ishq.local>
2018-03-14 02:04:59 +1100

    fixes

--- runcible2.py
+++ runcible2.py
@@ -238,7 +238,7 @@ class Runcible(monome.App):
         asyncio.async(self.play())
 
     def next_step(self, track, parameter):
-       #print("track.pos_mul: ", parameter, track.pos_mul[parameter])
+       print("track.pos_mul: ", parameter, track.pos_mul[parameter])
        track.pos_mul[parameter] = int(track.pos_mul[parameter]) + 1
 
        if track.pos_mul[parameter] >= self.current_pattern.tracks[track.track_id].tmul[parameter]:
