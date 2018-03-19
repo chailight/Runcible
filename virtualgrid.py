3717e280301eaa202dda78d12c1d2df25a09ed9a
Mark Pedersen <markp@ishq.local>
2018-03-19 23:20:31 +1100

    fixes to grid data splitting

--- virtualgrid.py
+++ virtualgrid.py
@@ -27,8 +27,8 @@ class VirtualGridWrapper(monome.GridWrapper):
         self.width = 16 
         #self.height = self.grid1.height #this assumes spanning only horizontally 
         self.height = 8 
-        #self.grid1.send('/sys/rotation 180')
-        #self.grid2.send('/sys/rotation 270')
+        #self.grid1.grid.send('/sys/rotation 180')
+        #self.grid2.grid.send('/sys/rotation 270')
         self.event_handler.on_grid_ready()
 
     def on_grid_key(self, x, y, s):
@@ -111,7 +111,7 @@ class VirtualGridWrapper(monome.GridWrapper):
         if len(data[0]) == 16:
             #need to split each row of data in half and then re-assemble into list of lists
             for i in range(8):
-                grid1_data[i]=data[i][0:8]
+                grid1_data[i]=data[i][0:7]
                 grid2_data[i]=data[i][8:]
             #print(grid1_data)
             #print(grid2_data)
