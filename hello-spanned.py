45b8be4ec564f6c5b26b4628dee7c12b52634d84
Mark Pedersen <markp@ishq.local>
2018-03-31 00:09:54 +1100

    fixes

--- hello-spanned.py
+++ hello-spanned.py
@@ -14,16 +14,20 @@ class Hello(monome.App):
         #print("connected to ", self.grid.id)
         #pass
 
-    @asyncio.coroutine
     def stop_chaser(self):
         self.chaser = 0
 
-    @asyncio.coroutine
     def start_chaser(self):
         self.chaser = 1
+        asyncio.async(self.run_chaser())
+
+    @asyncio.coroutine
+    def run_chaser(self):
         while (self.chaser == 1) :
             print(self.current_pos)
             self.current_pos = (self.current_pos + 1)%16
+            asyncio.sleep(1)
+
 
     def on_grid_key(self, x, y, s):
         print(x,y)
@@ -113,10 +117,10 @@ class Hello(monome.App):
             self.grid.led_map(0,0,data5)
 
         if x==1 and y==0:
-            asyncio.async(self.start_chaser())
+            self.start_chaser()
 
         if x==2 and y==0:
-            asyncio.async(self.stop_chaser())
+            self.stop_chaser()
 
 
 
