080a13e477da7bda30a0e28e52f01d81fc8bfa0c
Mark Pedersen <markp@ishq.local>
2018-03-31 22:35:34 +1100

    fixes

--- hello-spanned.py
+++ hello-spanned.py
@@ -37,7 +37,7 @@ class Hello(monome.App):
         while (self.chaser == 1) :
             print(self.current_pos)
             print(self.my_pos_buffer)
-            self.my_buffer.led_map(0,0,(np.bitwise_or(self.my_pos_buffer.levels),(self.my_buffer.levels/15))*15))
+            self.my_buffer.led_map(0,0,(np.bitwise_or(self.my_pos_buffer.levels,(self.my_buffer.levels/15))*15))
             np.roll(self.my_pos_buffer,1)
             self.current_pos = (self.current_pos + 1)%16
             #self.my_buffer.led_set(self.current_pos-1,7,0)
