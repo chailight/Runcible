93a6124335b13be131e6fced7b63dea3c85c67d6
Mark Pedersen <markp@ishq.local>
2018-03-31 17:41:54 +1100

    fixes

--- virtualgrid.py
+++ virtualgrid.py
@@ -298,7 +298,7 @@ class myGridBuffer:
 
     def led_level_set(self, x, y, l):
         if x < self.width and y < self.height:
-            print self.levels.shape
+            print("buffer led_level_set",x,y,self.levels.shape)
             self.levels[x][y] = l
 
     def led_level_all(self, l):
