94eb2cd9d5dd069a90d65d92af6bccf9841b2509
Mark Pedersen <markp@ishq.local>
2018-04-02 00:54:59 +1000

    fixes

--- hello-spanned.py
+++ hello-spanned.py
@@ -45,7 +45,7 @@ class Hello(monome.App):
         if y < 4:
             negative = np.ones((1,4-y),int)
             blank_top_section = np.zeros((1,4),int)
-            blank_bottom_section = np.zeros((1,y,int))
+            blank_bottom_section = np.zeros((1,y),int)
             octave_col = np.block([blank_top_section,negative,blank_bottom_section])
             print(octave_col)
             self.my_buffer.led_col(x,0,octave_col[0])
