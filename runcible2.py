a4dbd1b5b9ed344d44ef380fd3dc95856740d927
Mark Pedersen <markp@ishq.local>
2018-04-06 01:00:59 +1000

    fixes

--- runcible2.py
+++ runcible2.py
@@ -627,9 +627,9 @@ class Runcible(monome.App):
 
     def draw_trigger_page(self):
         #draw scale toggles
-        self.my_scale_toggle_buffer = np.concatenate((np.array([[self.current_pattern.tracks[0].scale_toggle,self.current_pattern.tracks[1].scale_toggle,self.current_pattern.tracks[2].scale_toggle,self.current_pattern.tracks[3].scale_toggle]]),np.array([[0,0,0,0,0,0,0,0,0,0,0,0]])))
+        self.my_scale_toggle_buffer = np.concatenate((np.array([[self.current_pattern.tracks[0].scale_toggle,self.current_pattern.tracks[1].scale_toggle,self.current_pattern.tracks[2].scale_toggle,self.current_pattern.tracks[3].scale_toggle]]),np.array([[0,0,0,0,0,0,0,0,0,0,0,0]])),axis=1)
         #draw mute toggles
-        self.my_mute_toggle_buffer = np.concatenate((np.array([[self.current_pattern.tracks[0].track_mute,self.current_pattern.tracks[1].scale_toggle,self.current_pattern.tracks[2].scale_toggle,self.current_pattern.tracks[3].scale_toggle]]),np.array([[0,0,0,0,0,0,0,0,0,0,0,0]])))
+        self.my_mute_toggle_buffer = np.concatenate((np.array([[self.current_pattern.tracks[0].track_mute,self.current_pattern.tracks[1].scale_toggle,self.current_pattern.tracks[2].scale_toggle,self.current_pattern.tracks[3].scale_toggle]]),np.array([[0,0,0,0,0,0,0,0,0,0,0,0]])),axis=1)
         self.my_buffer.led_row(0,1,self.my_mute_toggle_buffer)
         self.my_buffer.led_row(0,2,self.my_scale_toggle_buffer)
 
