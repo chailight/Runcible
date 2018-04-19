b62bee3272351535396d792a7194634d6ec99bba
Mark Pedersen <markp@ishq.local>
2018-04-20 00:15:10 +1000

    fixes

--- runcible2.py
+++ runcible2.py
@@ -581,6 +581,9 @@ class Runcible(monome.App):
                         self.my_buffer.led_row(0,7,np.roll((self.my_pos_buffer).astype(int),self.current_track.tmul[self.k_mode.value],axis=1))
                     elif self.k_mod_mode == ModModes.modLoop:
                         print("track",self.current_track.track_id,"lstart",self.current_track.lstart[self.k_mode.value],"lend",self.current_track.lend[self.k_mode.value])
+                        print("start",np.zeros(self.current_track.lstart[self.k_mode.value]).shape)
+                        print("middle",np.ones(self.current_track.lend[self.k_mode.value]+1).shape)
+                        print("end",np.zeros(15-self.current_track.lend[self.k_mode.value]).shape)
                         self.my_buffer.led_row(0,7,np.block([np.zeros(self.current_track.lstart[self.k_mode.value],int),np.ones(self.current_track.lend[self.k_mode.value]+1,int),np.zeros(15-self.current_track.lend[self.k_mode.value],int)]).tolist())
                         #    for i in range(16):
                         #        if i >= self.current_track.lstart[self.k_mode.value] and i <= self.current_track.lend[self.k_mode.value]:
